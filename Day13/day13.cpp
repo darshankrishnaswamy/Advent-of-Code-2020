9298998fdd092b47d82e679b2b2ec127a46fb55e
Darshan Krishnaswamy <darsh797@gmail.com>
2020-12-13 21:06:47 -0500

    Day 13 update

--- Day13/day13.cpp
+++ Day13/day13.cpp
@@ -2,12 +2,25 @@
 #include <fstream>
 #include <vector>
 #include <map>
+#include <numeric>
+#include <algorithm>
+
+#include <algorithm>
+#include <cassert>
+#include <charconv>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <vector>
+
 using namespace std;
 
 vector<int> times;
 int time;
 string s;
 map<int, int> m;
+
 void load_data(){
     ifstream fin("day13.txt");
     fin >> time;
@@ -18,7 +31,7 @@ void load_data(){
     int prev = 0;
     int count = 0;
     for(int i = 0; i < s.size(); i++){
-        if(s[i] == ','){
+        if(s[i] == ',') {
 
             if(s.substr(prev, i-prev) != "x") {
                 times.push_back(stoi(s.substr(prev, i - prev)));
@@ -47,23 +60,35 @@ int part1(){
     }
     return minTime * minBus;
 }
-
-long long myMod(int a, int b){
+/**
+ * Returns the positive modulo of a and b
+ * @param a the first number
+ * @param b the second number
+ * @return the positive module of a and b
+ */
+long long posMod(int a, int b){
     long long out = a % b;
     if(out < 0) out += b;
     return out;
 }
+long long gcd(long long a, long long b){
+    if(b == 0) return a;
+    return gcd(b, a%b);
+}
+long long lcm(long long a, long long b){
+    return a*b/gcd(a, b);
+}
 
+// uses Chinese Remainder Theorem to find the smallest value "finalTime" satisfying each of the mod conditions
 long long part2(){
     long long finalTime = times[0];
     long long multiplier = times[0];
 
     for(int i = 1; i < times.size(); i++){
-        int n = 0;
-        while(finalTime % times[i] != myMod((times[i]-m[times[i]]), times[i]))
+        while(finalTime % times[i] != posMod((times[i]-m[times[i]]), times[i]))
             finalTime += multiplier;
 
-        multiplier *= times[i];
+        multiplier = lcm(multiplier, times[i]);
     }
     return finalTime;
 }
